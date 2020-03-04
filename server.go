713e3325155ba6a22f62bf0d512ae84deb291cab
Malcolm Seyd <56709101+malcolmseyd@users.noreply.github.com>
2020-03-03 23:19:19 -0800

    Fix typo in server.go
    It wasn't compiling due to a typo in "NewExecutableSchema". I corrected it.

--- server.go
+++ server.go
@@ -19,7 +19,7 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExescutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
