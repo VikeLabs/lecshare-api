3ff129e733c286137c2cd96157770c746e92207c
Aomi Jokoji <aomi@oimo.ca>
2020-03-12 20:41:43 -0700

    add CORS middleware to AWS Lambda GraphQL endpoint

--- graph/middleware.go
+++ graph/middleware.go
@@ -5,10 +5,20 @@ import (
 	"net/http"
 )
 
+// Middleware is an example usage of a simple middlware.
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), "Message", "Hello World")
 		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// CorsMiddleware is a help function get around CORS
+// TODO do this better
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
