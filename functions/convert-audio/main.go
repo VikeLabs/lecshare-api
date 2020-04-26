17e734e9a7a7b1a2e396fca42071327e10e059cb
Malcolm Seyd <56709101+malcolmseyd@users.noreply.github.com>
2020-04-25 22:24:31 -0700

    Transcript integration (#19)
    * integrate Derek's transcription script with Lambda
    * create tests and make functions more consistent
    
    debugging print statements -_-
    
    exclude jobName as it caused errors
    
    get debug info from Lamba instance
    
    don't use URLEncodedKey, it's empty
    
    create intermediate transcription bucket & lambda handler
    
    existing: true on transcription bucket
    
    enable rw permissions on transcription bucket
    
    bad indentation :(
    
    make the waitgroup actually wait
    
    copy json instead of down/upload
    
    change to base64 and cut extra code
    
    clean up
    
    give delete permissions in transcription bucket
    
    disable delete, it times out costing us $$$
    
    it works, we can worry about temp files later

--- functions/convert-audio/main.go
+++ functions/convert-audio/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"log"
@@ -19,9 +20,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/transcribeservice"
 )
 
 var ffmpegDir string
+
+const testingBucket = "assets-lecshare.oimo.ca"
+const transcriptionBucket = "lecshare-transcriptions"
 
 // Only use this with downloader.Concurrency = 1, otherwise it will break.
 type fakeWriterAt struct {
@@ -56,7 +61,7 @@ func downloadS3(key string, bucket string, outPipe *io.PipeWriter, wg *sync.Wait
 	wg.Done()
 }
 
-func uploadS3(key string, oldKey string, bitrate int, inPipe *io.PipeReader, wg *sync.WaitGroup) {
+func uploadS3(key string, oldKey string, bucket string, bitrate int, inPipe *io.PipeReader, wg *sync.WaitGroup) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-west-2")},
 	)
@@ -64,7 +69,7 @@ func uploadS3(key string, oldKey string, bitrate int, inPipe *io.PipeReader, wg 
 	uploader := s3manager.NewUploader(sess)
 
 	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket:      aws.String("assets-lecshare.oimo.ca"),
+		Bucket:      aws.String(bucket),
 		Key:         aws.String(key),
 		Body:        inPipe,
 		ContentType: aws.String("audio/ogg"),
@@ -107,7 +112,7 @@ func encodeAudio(bitrate int, inPipe *io.PipeReader, outPipe *io.PipeWriter, wg 
 func processAudio(key string, s3object events.S3Entity) {
 	bitrate := 128
 
-	outKey := strings.TrimSuffix(key, path.Ext(key)) + "-compressed.ogg"
+	outKey := strings.TrimSuffix(key, path.Ext(key)) + ".ogg"
 
 	inRead, inWrite := io.Pipe()
 	outRead, outWrite := io.Pipe()
@@ -117,9 +122,52 @@ func processAudio(key string, s3object events.S3Entity) {
 	wg.Add(3)
 	go downloadS3(key, s3object.Bucket.Name, inWrite, &wg)
 	go encodeAudio(bitrate, inRead, outWrite, &wg)
-	go uploadS3(outKey, key, bitrate, outRead, &wg)
+	go uploadS3(outKey, key, testingBucket, bitrate, outRead, &wg)
 	fmt.Print("\n")
 	wg.Wait()
+}
+
+func transcribeAudio(key string, s3object events.S3Entity) {
+	// encode original key in jobName so we can smuggle it into the output
+	// no padding because jobName allows a limited char set: ^[0-9a-zA-Z._-]+ up to 200 chars
+	enc := base64.URLEncoding.WithPadding(base64.NoPadding)
+
+	jobName := enc.EncodeToString([]byte(strings.TrimSuffix(key, path.Ext(key))))
+	jobURI := "s3://" + s3object.Bucket.Name + "/" + s3object.Object.Key
+	outBucket := transcriptionBucket
+
+	// open a new session
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2"),
+	})
+	fmt.Println(">> Opening Transcribe session for", key)
+	transcriber := transcribeservice.New(sess)
+	// exit if unable to create a Transcribe session
+	if transcriber == nil {
+		log.Fatalln("Unable to create Transcribe session")
+	} else {
+		fmt.Println(">> Transcribe session successfully created")
+	}
+
+	mediaformat := "flac"
+	languagecode := "en-US"
+	var StrucMedia transcribeservice.Media
+	StrucMedia.MediaFileUri = &jobURI
+
+	fmt.Println(">> Creating transcription job")
+
+	_, err := transcriber.StartTranscriptionJob(&transcribeservice.StartTranscriptionJobInput{
+		TranscriptionJobName: &jobName,
+		Media:                &StrucMedia,
+		MediaFormat:          &mediaformat,
+		LanguageCode:         &languagecode,
+		OutputBucketName:     &outBucket,
+	})
+	if err != nil {
+		log.Fatalln("Got error building project: ", err)
+	}
+
+	fmt.Println("Successfully created transcription job for", key)
 }
 
 func newAudioHandler(ctx context.Context, event events.S3Event) error {
@@ -128,7 +176,9 @@ func newAudioHandler(ctx context.Context, event events.S3Event) error {
 		if err != nil {
 			return err
 		}
-		fmt.Println("Processing ", key)
+		fmt.Println("Transcribing", key)
+		transcribeAudio(key, r.S3)
+		fmt.Println("Processing", key)
 		processAudio(key, r.S3)
 	}
 
